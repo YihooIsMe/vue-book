00d46b198e3dd970607c3370a9586226d8d07eaa
YihooIsMe <yihoo2016@gmail.com>
2018-03-29 17:59:00 +0800

    add project and component

--- html/Directive/project/index.js
+++ html/Directive/project/index.js
@@ -5,76 +5,120 @@ var app=new Vue({
            {
                id:1,
                name:"iPhone 7",
-               price:6188,
+               price:5000,
                count:1
            },
            {
                id:2,
                name:"iPad Pro",
-               price:5888,
+               price:1000,
                count:1
            },
            {
                id:3,
                name:"MacBook Pro",
-               price:21488,
+               price:10000,
                count:1
            }
        ],
-        checkModel:true,
-        forChecks:[1,2,3]
+        forChecks:[],
+        totalPrice:[]
+    },
+    created:function () {
+        var _this=this;
+        this.list.forEach(function (forItem) {
+            _this.forChecks.push(forItem.id);
+            _this.totalPrice.push(forItem.price*forItem.count);
+            _this.$set(forItem,"checked",true);
+        });
     },
     methods:{
-        handleReduce:function (index) {
+        handleReduce:function (item,index) {
             if(this.list[index].count===1) return;
             this.list[index].count--;
+            this.addOrReduce();
         },
-        handleAdd:function (index) {
+        handleAdd:function (item,index) {
             this.list[index].count++;
+            this.addOrReduce();
         },
-        handleRemove:function (index) {
-            this.list.splice(index,1);
-        },
-        checkAll:function () {
+        handleRemove:function (item,index) {
             var _this=this;
-            if(this.checkModel){
-                this.forChecks=[];
-            }else {
-                this.forChecks=[];
-                this.list.forEach(function (item) {
-                    _this.forChecks.push(item.id);
-                })
-            }
-
-        },
-        changSelect:function (index) {
-
-        }
-    },
-    watch:{
-        "forChecks":{
-            handler:function (val,oldVal) {
-                console.log(val);
-                console.log(oldVal);
-                if(this.forChecks.length===this.list.length){
-                    this.checkModel=true;
-                }else {
-                    var otherList=this.list.concat();
-                    this.checkModel=false;
-                    this.list.splice(0,1);
+            this.list.splice(index,1);
+            this.totalPrice.splice(index,1);
+            this.forChecks.forEach(function (forItem,forIndex) {
+                if(forItem==item.id){
+                    _this.forChecks.splice(forIndex,1);
                 }
-
-            },
-            deep:true
+            });
+            this.addOrReduce();
+        },
+        changSelect:function (item) {
+            let _this=this;
+            if(typeof item.checked=='undefined'){
+                this.totalPrice=[];
+                this.$set(item,"checked",true);
+                let total=0;
+                this.list.forEach(function (forItem) {
+                    if(forItem.checked){
+                        total+=forItem.price*forItem.count;
+                    }
+                });
+                this.totalPrice.push(total);
+            }else {
+                this.totalPrice=[];
+                this.$delete(item,"checked");
+                let total=0;
+                this.list.forEach(function (forItem) {
+                    if(forItem.checked){
+                        total+=forItem.price*forItem.count;
+                    }
+                });
+                this.totalPrice.push(total);
+            }
+        },
+        addOrReduce:function () {
+            var _this=this;
+            this.totalPrice=[];
+            this.list.forEach(function (forItem) {
+                if(forItem.checked){
+                    _this.totalPrice.push(forItem.price*forItem.count)
+                }
+            })
         }
     },
     computed:{
-        totalPrice:function () {
+        checkAll:{
+          get:function () {
+              return this.forChecks.length===this.list.length;
+          },
+          set:function (v) {
+              var _this=this;
+              if(v){
+                  this.forChecks=[];
+                  this.list.forEach(function (forItem, index) {
+                      _this.forChecks.push(forItem.id);
+                      _this.$set(forItem,"checked",true);
+                  });
+                  this.totalPrice=[];
+                  this.list.forEach(function (forItem) {
+                      _this.totalPrice.push(forItem.price*forItem.count)
+                  })
+              }else {
+                  this.forChecks=[];
+                  this.totalPrice=[];
+                  this.list.forEach(function (forItem, index) {
+                      _this.$delete(forItem,"checked");
+                  });
+              }
+          }  
+        },
+        totalMoney:function () {
             var total=0;
-            for(var i=0;i<this.list.length;i++){
-                var item=this.list[i];
-                total+=item.price*item.count;
+            for(var i=0;i<this.totalPrice.length;i++){
+                total+=this.totalPrice[i];
             }
+            console.log(total);
             return total.toString().replace(/\B(?=(\d{3})+$)/g,",")
         }
     }
