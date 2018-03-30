7cbf20e7f4e1bd04aac115c645181c52c8365418
YihooIsMe <yihoo2016@gmail.com>
2018-03-30 18:02:52 +0800

    add slot

---
+++ html/Components/a.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <slot name="book" v-for="book in books" :book-name="book.name"></slot>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "a"
+    }
+</script>
+
+<style scoped>
+
+</style>
